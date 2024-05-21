33e4aafc85c481ab9665602ede363cf9d93d9630
Nick: The Dev <74797598+ntnicholson@users.noreply.github.com>
2024-05-21 08:43:15 -0500

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Nicholas Nicholson on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
