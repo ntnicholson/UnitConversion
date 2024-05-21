622072741b884b48b48ae6f92e1cdea02bc718b8
Nick: The Dev <74797598+ntnicholson@users.noreply.github.com>
2024-05-21 10:17:06 -0500

    Update ContentView.swift
    HackingWithSwift: Project 1 - Day 19, Unit Conversion

--- UnitConversion/ContentView.swift
+++ UnitConversion/ContentView.swift
@@ -8,14 +8,99 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @State private var inputNumber = 0
+    @State private var inputUnit = "m"
+    @State private var outputUnit = "m"
+    @State private var outputNumber = 0
+    
+    var convertedValue: Measurement<UnitLength> {
+        let baseValue = SetBaseValue(input: inputUnit)
+        //
+        var convertedValue = baseValue
+        
+        if (outputUnit == UnitLength.meters.symbol){
+            convertedValue = baseValue.converted(to: UnitLength.meters)
         }
-        .padding()
+        else if (outputUnit == UnitLength.kilometers.symbol){
+            convertedValue = baseValue.converted(to: UnitLength.kilometers)
+        }
+        else if (outputUnit == UnitLength.feet.symbol){
+            convertedValue = baseValue.converted(to: UnitLength.feet)
+        }
+        else if (outputUnit == UnitLength.yards.symbol){
+            convertedValue = baseValue.converted(to: UnitLength.yards)
+        }
+        else if (outputUnit == UnitLength.miles.symbol){
+            convertedValue = baseValue.converted(to: UnitLength.miles)
+        }
+        
+        return convertedValue
+    }
+    
+    //let temperatureConversion = ["Celsius", "Fahrenheit", "Kelvin"]
+    let lengthConvert = [UnitLength.meters.symbol, UnitLength.kilometers.symbol, UnitLength.feet.symbol, UnitLength.yards.symbol, UnitLength.miles.symbol]
+    //let timeConversion = ["Seconds", "Minutes", "Hours", "Days"]
+    
+    
+    func SetBaseValue(input: String) -> Measurement<UnitLength> {
+        var baseValue = Measurement(value: Double(inputNumber), unit: UnitLength.meters)
+        
+        if (input == UnitLength.meters.symbol){
+           baseValue =  Measurement(value: Double(inputNumber), unit: UnitLength.meters)
+        }
+        else if (input == UnitLength.kilometers.symbol){
+            baseValue = Measurement(value: Double(inputNumber), unit: UnitLength.kilometers)
+        }
+        else if (input == UnitLength.feet.symbol){
+            baseValue = Measurement(value: Double(inputNumber), unit: UnitLength.feet)
+
+        }
+        else if (input == UnitLength.yards.symbol){
+            baseValue = Measurement(value: Double(inputNumber), unit: UnitLength.meters)
+
+        }
+        else if (input == UnitLength.miles.symbol){
+            baseValue = Measurement(value: Double(inputNumber), unit: UnitLength.miles)
+
+        }
+        
+        return baseValue
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form{
+                Section{
+                    Picker("Input Unit", selection: $inputUnit) {
+                        ForEach(lengthConvert, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    Picker("Output Unit", selection: $outputUnit) {
+                        ForEach(lengthConvert, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    
+                }
+                Section("Input A Value"){
+                    HStack{
+                        TextField("Input", value: $inputNumber, format: .number).keyboardType(.decimalPad)
+                        Text("\(inputUnit)")
+                    }
+                    
+                }
+                Section("Converted Value"){
+                    HStack{
+                        Text("\(convertedValue.value)")
+                        Text("\(convertedValue.unit.symbol)")
+                    }
+                }
+                .navigationTitle("Length Conversion")
+            }
+            
+        }
     }
 }
 
